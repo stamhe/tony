748509c0fd36deba6569117220e0bf7eb62bb27e
qihaier <qihaier@qq.com>
2014-05-22 15:51:59 +0800

    define

---
+++ const.php
@@ -0,0 +1,16 @@
+<?php
+//定义数据库常量
+define('DBHOST','localhost');
+define('DBUSER','root');
+define('DBPWD','root');
+define('DBPORT','3306');
+define('DBCHARSET','utf8');
+//表前缀
+define('DBPREFIX','pg_');
+//主表的前缀
+define('PRIMARY_DBPREFIX','bg_');
+define('DBNAME','exam');
+//页面编码
+define('CHARSET','utf-8');
+
+
